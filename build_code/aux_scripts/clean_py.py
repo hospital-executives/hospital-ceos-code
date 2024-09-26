6f9baec6a76f0b53eb2cdcd483928e8e2cd3651e
kpapen <katherinepapen@uchicago.edu>
2024-09-26 12:48:19 -0500

    set up branch to work on confirmed cases

--- build_code/aux_scripts/clean_py.py
+++ build_code/aux_scripts/clean_py.py
@@ -1,9 +1,11 @@
 import os
+import sys
 import pandas as pd
 
 user_path = "/Users/loaner/BFI Dropbox/Katherine Papen/hospital_ceos/_data"
 
-from ..helper-scripts import cleaned_confirmed_helper as cc
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from helper_scripts import cleaned_confirmed_helper as cc
 
 # load files
 confirmed_path =  os.path.join(user_path, "derived/auxiliary/py_confirmed.csv")
@@ -12,3 +14,35 @@ remainder_path =  os.path.join(user_path, "derived/auxiliary/py_remaining.csv")
 py_confirmed = pd.read_csv(confirmed_path)
 py_remainder = pd.read_csv(remainder_path)
 
+
+name_pairs_set, meta_pairs_set = cc.gen_name_meta_pairs(user_path)
+
+new_grouped = py_remainder.groupby(['last_meta', 'first_component'])
+pair_results = pd.concat([cc.find_pairwise_shared_attributes(sub_df, 
+                                                             name_pairs_set,
+                                                             meta_pairs_set) 
+                                                             for _, 
+                     sub_df in new_grouped]) 
+
+pair_results = cc.update_results(pair_results) 
+contact_dict, contact_count_dict = cc.generate_pair_dicts(pair_results)
+
+confirmed_graph, cleaned_dropped1, cleaned_remaining1 = cc.clean_results_pt1(
+    pair_results)
+
+cc.update_confirmed_from_dropped(confirmed_graph, cleaned_dropped1,
+                                                 contact_count_dict)
+
+cleaned_dropped2, cleaned_remaining2 = cc.clean_results_pt2(
+    cleaned_remaining1, confirmed_graph, cleaned_dropped1, new_himss)
+
+cc.update_confirmed_from_dropped(confirmed_graph, cleaned_dropped2,
+                                                 contact_count_dict)
+
+remaining_ids = pd.concat([cleaned_remaining2['contact_id1'],
+                             cleaned_remaining2['contact_id2']])
+remaining_ids_from_confirmed = set(remaining_ids.unique())
+
+py_remainder['contact_uniqueid'] = py_remainder['contact_uniqueid'].astype(str)
+test = py_remainder[~py_remainder['contact_uniqueid'].isin(remaining_ids_from_confirmed)]
+print(len(test))
---
+++ build_code/aux_scripts/fuzzy_all.py
@@ -0,0 +1,164 @@
+import os
+import sys 
+import json
+import pandas as pd
+import networkx as nx
+from itertools import chain
+
+user_path = "/Users/loaner/BFI Dropbox/Katherine Papen/hospital_ceos/_data"
+# TAKE ARGS FROM MAKEFILE
+# Get arguments from the command line
+if len(sys.argv) == 8:
+    confirmed_path = sys.argv[1]
+    remaining_path = sys.argv[2]
+
+    final_cleaned_path = sys.argv[3]
+    final_remaining_path = sys.argv[4]
+    components_path = sys.argv[5]
+    user_path = sys.argv[6] # data dir
+    code_path = sys.argv[7]
+else: 
+    confirmed_path = os.path.join(user_path, "derived/auxiliary/confirmed_2.csv")
+    remaining_path =  os.path.join(user_path, "derived/auxiliary/remaining_2.csv")
+    final_cleaned_path =  os.path.join(user_path, "derived/py_confirmed.csv")
+    final_remaining_path =  os.path.join(user_path, "derived/py_remaining.csv")
+    components_path =  os.path.join(user_path, "derived/py_graph_components.json")
+    code_path = os.getcwd()
+
+#raise ValueError
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from helper_scripts import cleaned_confirmed_helper as cc
+from helper_scripts import blocking_helper
+
+# load dfs and set variable(s)
+cleaned_confirmed = pd.read_csv(confirmed_path)
+cleaned_remaining = pd.read_csv(remaining_path)
+new_himss = pd.concat([cleaned_confirmed, cleaned_remaining], axis=0)
+regenerate_hyphenated_pairs = None
+
+outliers_path = os.path.join(user_path, "derived/auxiliary/outliers.csv")
+outliers = pd.read_csv(outliers_path)
+outlier_ids = set(outliers['contact_uniqueid'])
+input_df = new_himss[~new_himss['contact_uniqueid'].isin(outlier_ids)]
+
+###################### LAST META AND FIRST COMPONENT ######################
+#### BLOCK LEVEL COMPARISONS ####
+print('block level')
+input_df['contact_uniqueid'] = input_df['contact_uniqueid'].apply(str)
+grouped = input_df.groupby(['last_meta', 'first_component'])
+block_results = grouped.apply(cc.find_common_blocks).reset_index() # takes 10 min
+
+# separate into confirmed/remaining
+cleaned1 = block_results[(block_results['contact_uniqueid'].apply(len) == 1)]
+remaining1 = block_results[(block_results['contact_uniqueid'].apply(len) > 1)]
+
+cleaned_ids = set(chain.from_iterable(cleaned1['contact_uniqueid']))
+remaining_ids = set(chain.from_iterable(remaining1['contact_uniqueid'])) # 110754
+unique_contact_ids = cleaned_ids - remaining_ids #83078
+
+# final remaining from blockwise comparisons
+filtered_df = input_df[input_df['contact_uniqueid'
+                                                  ].isin(remaining_ids)]
+
+name_pairs_set, meta_pairs_set = cc.gen_name_meta_pairs(user_path)
+
+#### PAIRWISE LEVEL COMPARISONS ####
+print('pairwise level')
+new_grouped = filtered_df.groupby(['last_meta', 'first_component'])
+pair_results = pd.concat([cc.find_pairwise_shared_attributes(sub_df, 
+                                                             name_pairs_set,
+                                                             meta_pairs_set) 
+                                                             for _, 
+                     sub_df in new_grouped]) # 25 min
+pair_results = cc.update_results(pair_results) # 110754 - correct
+
+contact_dict, contact_count_dict = cc.generate_pair_dicts(pair_results)
+
+confirmed_graph, cleaned_dropped1, cleaned_remaining1 = cc.clean_results_pt1(
+    pair_results, unique_contact_ids)
+
+cc.update_confirmed_from_dropped(confirmed_graph, cleaned_dropped1,
+                                                 contact_count_dict)
+
+cleaned_dropped2, cleaned_remaining2 = cc.clean_results_pt2(
+    cleaned_remaining1, confirmed_graph, cleaned_dropped1, new_himss)
+
+cc.update_confirmed_from_dropped(confirmed_graph, cleaned_dropped2,
+                                                 contact_count_dict)
+
+cleaned_dropped3, cleaned_remaining3 = cc.clean_results_pt3(
+    cleaned_remaining2, cleaned_dropped2, user_path, new_himss, 
+    cleaned_confirmed) # these are the remaining from the confirmed df 
+
+cc.update_confirmed_from_dropped(confirmed_graph, cleaned_dropped3,
+                                                 contact_count_dict)
+
+## determine final confirmed/remaining
+remaining_ids = pd.concat([cleaned_remaining3['contact_id1'],
+                             cleaned_remaining3['contact_id2']])
+remaining_ids_from_confirmed = set(remaining_ids.unique())
+cleaned_ids_from_confirmed = set(confirmed_graph.nodes()) -  remaining_ids_from_confirmed
+
+
+subgraph = confirmed_graph.subgraph(cleaned_ids_from_confirmed).copy()
+cleaned_df = input_df[~input_df['contact_uniqueid'].isin(cleaned_ids_from_confirmed)]
+
+# now need to test ids that werent there orginally
+py_ids = set(py_confirmed['contact_uniqueid'])
+new_cleaned = cleaned_ids_from_confirmed - py_ids
+
+# let's see
+new_grouped = cleaned_df.groupby(['last_meta', 'first_meta'])
+pair_results_first = pd.concat([cc.find_pairwise_shared_attributes(sub_df, 
+                                                             name_pairs_set,
+                                                             meta_pairs_set) 
+                                                             for _, 
+                     sub_df in new_grouped])
+pair_results_first = cc.update_results(pair_results_first) 
+merged_df = pd.merge(pair_results_first
+, pair_results, on=['last_meta', 'contact_id1', 'contact_id2'], how='left', indicator=True)
+test = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
+test = test.loc[:, ~test.columns.str.endswith('_y')]
+test.columns = test.columns.str.replace('_x$', '', regex=True)
+
+## need to determine which cases are clear
+confirmed_ids = set()
+remaining_ids_in_pairs = set(test['contact_id1']).union(set(test['contact_id2']))
+for id_ in final_confirmed_ids:
+    if id_ not in remaining_ids_in_pairs:
+        confirmed_ids.add(id_)
+
+
+contact_dict, contact_count_dict = cc.generate_pair_dicts(test)
+
+test_graph, cleaned_dropped1, cleaned_remaining1 = cc.clean_results_pt1(test)
+
+cc.update_confirmed_from_dropped(test_graph, cleaned_dropped1,
+                                                 contact_count_dict)
+
+cleaned_dropped2, cleaned_remaining2 = cc.clean_results_pt2(
+    cleaned_remaining1, test_graph, cleaned_dropped1, new_himss)
+
+cc.update_confirmed_from_dropped(test_graph, cleaned_dropped2,
+                                                 contact_count_dict)
+
+cleaned_dropped3, cleaned_remaining3 = cc.clean_results_pt3(
+    cleaned_remaining2, cleaned_dropped2, user_path, new_himss, 
+    cleaned_confirmed) # these are the remaining from the confirmed df 
+
+cc.update_confirmed_from_dropped(test_graph, cleaned_dropped3,
+                                                 contact_count_dict)
+
+remaining_ids = set(final_confirmed['contact_uniqueid'].unique()) - \
+                confirmed_ids - set(test_graph.nodes())
+
+common_nodes =  set(final_confirmed['contact_uniqueid'].unique()
+                    ).union(confirmed_ids) - remaining_ids
+
+G1_subgraph = confirmed_graph.subgraph(common_nodes).copy()
+G_combined = nx.compose(G1_subgraph, test_graph)
+# 163k IDs
+# with 162k "people"
+final_confirmed_2 = final_confirmed[~final_confirmed['contact_uniqueid'].isin(remaining_ids)]
+final_confirmed_ids_2 = set(final_confirmed_2['contact_uniqueid'])
